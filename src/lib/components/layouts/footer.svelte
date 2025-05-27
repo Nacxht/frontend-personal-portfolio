590b5af896b3df41e7c12570878f1419b5e20575
Nacxht <zombiecrafter069@gmail.com>
2025-05-27 16:22:39 +0700

    feat: added footer

---
+++ src/lib/components/layouts/footer.svelte
@@ -0,0 +1,57 @@
+<script>
+	//
+</script>
+
+<footer>
+	<div class="up-content">
+		<a href="https://github.com/Nacxht" target="_blank" class="nickname">zkyzors</a>
+
+		<p class="purpose">A reflection of my journey as developer</p>
+	</div>
+
+	<hr class="divider" />
+
+	<div class="copyright">
+		<p>&copy; 2025 ZkyZors. All Rights Reserved</p>
+	</div>
+</footer>
+
+<style lang="postcss">
+	@reference "tailwindcss";
+
+	footer {
+		@apply grid gap-[2rem] py-[2rem] px-[1rem] lg:px-[10rem];
+	}
+
+	:global(.light) footer {
+		@apply border-t border-gray-300;
+	}
+
+	:global(.dark) footer {
+		@apply border-t border-gray-800;
+	}
+
+	.up-content {
+		@apply flex justify-between items-center;
+	}
+
+	.up-content .nickname {
+		@apply text-lg lg:text-2xl font-bold uppercase;
+	}
+
+	:global(.light) .up-content .nickname {
+		@apply text-gray-950;
+	}
+
+	:global(.dark) .up-content .nickname {
+		@apply text-gray-50;
+	}
+
+	.up-content .purpose {
+		@apply italic text-xs lg:text-sm;
+	}
+
+	.copyright {
+		@apply flex justify-center text-xs lg:text-sm;
+	}
+</style>
