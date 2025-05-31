595ba9e9279f632fe54fc81d500378c3573aea48
Nacxht <zombiecrafter069@gmail.com>
2025-06-01 02:04:13 +0700

    feat: initialize pagination component

---
+++ src/lib/components/reusable/pagination/pagination.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Button from "../button/button.svelte";
+	import { ButtonVariant } from "../button/variant";
+	import { ChevronLeft, ChevronRight } from "lucide-svelte";
+
+	//
+</script>
+
+<div class="pagination">
+	<Button variant={ButtonVariant.GHOST}>
+		<ChevronLeft />
+		Previous
+	</Button>
+
+	<!--  -->
+
+	<Button variant={ButtonVariant.GHOST}>
+		Next
+		<ChevronRight />
+	</Button>
+</div>
+
+<style lang="postcss">
+	@reference "tailwindcss";
+</style>
