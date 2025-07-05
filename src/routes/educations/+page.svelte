052adbf4cf6c5cb23f77cf7aab0272483102a74f
Nacxht <zombiecrafter069@gmail.com>
2025-07-05 17:38:40 +0700

    feat: added content of education's page

---
+++ src/routes/educations/+page.svelte
@@ -0,0 +1,75 @@
+<script>
+	import Steps from "$lib/components/reusable/steps/steps.svelte";
+</script>
+
+<main>
+	<div class="container">
+		<section class="title">
+			<h1>educations</h1>
+		</section>
+
+		<hr class="divider" />
+
+		<section id="educations">
+			<div class="step">
+				<Steps title={"School at SMK Krian 1"} date={"2019 - 2023"}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime neque reprehenderit vero
+					culpa. Eos alias aliquid quam numquam ipsum sapiente fugit repellendus distinctio atque
+					officiis corporis rem porro doloremque laudantium commodi repellat reiciendis molestias,
+					inventore vel? Nobis, non quasi. Aperiam et dignissimos debitis deserunt accusantium sint
+					beatae hic labore rerum porro doloribus non, unde ad similique, odio ipsam. Perspiciatis
+					itaque aspernatur illum quae? Qui quae incidunt repudiandae minus perferendis
+					necessitatibus voluptatibus illo odio. Quas asperiores quod similique vero exercitationem
+					corrupti, recusandae esse dolore accusamus odit deserunt rerum nam suscipit nulla neque
+					harum sed quaerat quia illo assumenda ipsam minus. Perspiciatis?
+				</Steps>
+			</div>
+
+			<div class="step">
+				<Steps title={"School at SMK Krian 1"} date={"2019 - 2023"}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime neque reprehenderit vero
+					culpa. Eos alias aliquid quam numquam ipsum sapiente fugit repellendus distinctio atque
+					officiis corporis rem porro doloremque laudantium commodi repellat reiciendis molestias,
+					inventore vel? Nobis, non quasi. Aperiam et dignissimos debitis deserunt accusantium sint
+					beatae hic labore rerum porro doloribus non, unde ad similique, odio ipsam. Perspiciatis
+					itaque aspernatur illum quae? Qui quae incidunt repudiandae minus perferendis
+					necessitatibus voluptatibus illo odio. Quas asperiores quod similique vero exercitationem
+					corrupti, recusandae esse dolore accusamus odit deserunt rerum nam suscipit nulla neque
+					harum sed quaerat quia illo assumenda ipsam minus. Perspiciatis?
+				</Steps>
+			</div>
+
+			<div class="step">
+				<Steps title={"School at SMK Krian 1"} date={"2019 - 2023"}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime neque reprehenderit vero
+					culpa. Eos alias aliquid quam numquam ipsum sapiente fugit repellendus distinctio atque
+					officiis corporis rem porro doloremque laudantium commodi repellat reiciendis molestias,
+					inventore vel? Nobis, non quasi. Aperiam et dignissimos debitis deserunt accusantium sint
+					beatae hic labore rerum porro doloribus non, unde ad similique, odio ipsam. Perspiciatis
+					itaque aspernatur illum quae? Qui quae incidunt repudiandae minus perferendis
+					necessitatibus voluptatibus illo odio. Quas asperiores quod similique vero exercitationem
+					corrupti, recusandae esse dolore accusamus odit deserunt rerum nam suscipit nulla neque
+					harum sed quaerat quia illo assumenda ipsam minus. Perspiciatis?
+				</Steps>
+			</div>
+
+			<div class="step">
+				<Steps title={"School at SMK Krian 1"} date={"2019 - 2023"}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime neque reprehenderit vero
+					culpa. Eos alias aliquid quam numquam ipsum sapiente fugit repellendus distinctio atque
+					officiis corporis rem porro doloremque laudantium commodi repellat reiciendis molestias,
+					inventore vel? Nobis, non quasi. Aperiam et dignissimos debitis deserunt accusantium sint
+					beatae hic labore rerum porro doloribus non, unde ad similique, odio ipsam. Perspiciatis
+					itaque aspernatur illum quae? Qui quae incidunt repudiandae minus perferendis
+					necessitatibus voluptatibus illo odio. Quas asperiores quod similique vero exercitationem
+					corrupti, recusandae esse dolore accusamus odit deserunt rerum nam suscipit nulla neque
+					harum sed quaerat quia illo assumenda ipsam minus. Perspiciatis?
+				</Steps>
+			</div>
+		</section>
+	</div>
+</main>
+
+<style lang="postcss">
+	@references "tailwindcss";
+</style>
