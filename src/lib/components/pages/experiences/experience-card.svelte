70caade2cc606eea85e0c0a53d4fafbeba230bfc
Nacxht <zombiecrafter069@gmail.com>
2025-07-06 17:37:18 +0700

    feat: added experiences page

---
+++ src/lib/components/pages/experiences/experience-card.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import Badge from "$lib/components/reusable/badge/badge.svelte";
+	import { BadgeVariant } from "$lib/components/reusable/badge/variant";
+	import Card from "$lib/components/reusable/card/card.svelte";
+
+	export let periodStart: string;
+	export let periodEnd: string | undefined = undefined;
+	export let category: string;
+</script>
+
+<Card>
+	<div class="container">
+		<div class="top-part">
+			<div class="title">
+				<h3>
+					<slot name="title" />
+				</h3>
+			</div>
+
+			<div class="years-category">
+				<p class="period">
+					{periodStart} - {periodEnd || "Now"}
+				</p>
+
+				<Badge
+					variant={BadgeVariant.CUSTOM}
+					customColor="bg-purple-500 dark:bg-purple-800 text-gray-50"
+				>
+					{category}
+				</Badge>
+			</div>
+		</div>
+
+		<hr class="divider" />
+
+		<div class="bottom-part">
+			<p class="description">
+				<slot name="description" />
+			</p>
+		</div>
+	</div>
+</Card>
+
+<style lang="postcss">
+	@reference "tailwindcss";
+
+	.container {
+		@apply gap-[1rem] p-[0.5rem] md:p-0;
+	}
+
+	.top-part {
+		@apply grid gap-[1rem];
+	}
+
+	.years-category {
+		@apply flex justify-between;
+	}
+</style>
---
+++ src/routes/experiences/+page.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+	import ExperienceCard from "$lib/components/pages/experiences/experience-card.svelte";
+	import Button from "$lib/components/reusable/button/button.svelte";
+	import { ButtonVariant } from "$lib/components/reusable/button/variant";
+	import TextNumber from "$lib/components/reusable/input/text-number/text-number.svelte";
+	import { TextNumberVariant } from "$lib/components/reusable/input/text-number/variant";
+	import Pagination from "$lib/components/reusable/pagination/pagination.svelte";
+</script>
+
+<main>
+	<div class="container">
+		<section id="page-title">
+			<h1>experiences</h1>
+		</section>
+
+		<hr class="divider" />
+
+		<section id="search-input">
+			<TextNumber
+				type={TextNumberVariant.TEXT}
+				name="search"
+				id="search"
+				width="w-full lg:w-[20rem]"
+				placeholder="Search experiences..."
+			/>
+
+			<Button variant={ButtonVariant.SECONDARY}>Search</Button>
+		</section>
+
+		<section id="experiences">
+			<div class="experiences-grid">
+				<ExperienceCard category="Internship" periodStart="2022" periodEnd="2023">
+					<div slot="title">Internship at PT. Pineleng Indah Cemerlang</div>
+
+					<div slot="description">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi explicabo nostrum
+						quibusdam, esse quo debitis, distinctio molestiae laborum excepturi pariatur est impedit
+						quas quos eos ipsa, voluptate quisquam iste quasi expedita. Hic incidunt totam
+						recusandae molestiae earum fugiat, necessitatibus saepe error sed dolorum numquam eum
+						ratione repellat dolor maxime est!
+					</div>
+				</ExperienceCard>
+
+				<ExperienceCard category="Internship" periodStart="2022" periodEnd="2023">
+					<div slot="title">Internship at PT. Pineleng Indah Cemerlang</div>
+
+					<div slot="description">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi explicabo nostrum
+						quibusdam, esse quo debitis, distinctio molestiae laborum excepturi pariatur est impedit
+						quas quos eos ipsa, voluptate quisquam iste quasi expedita. Hic incidunt totam
+						recusandae molestiae earum fugiat, necessitatibus saepe error sed dolorum numquam eum
+						ratione repellat dolor maxime est!
+					</div>
+				</ExperienceCard>
+
+				<ExperienceCard category="Internship" periodStart="2022" periodEnd="2023">
+					<div slot="title">Internship at PT. Pineleng Indah Cemerlang</div>
+
+					<div slot="description">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi explicabo nostrum
+						quibusdam, esse quo debitis, distinctio molestiae laborum excepturi pariatur est impedit
+						quas quos eos ipsa, voluptate quisquam iste quasi expedita. Hic incidunt totam
+						recusandae molestiae earum fugiat, necessitatibus saepe error sed dolorum numquam eum
+						ratione repellat dolor maxime est!
+					</div>
+				</ExperienceCard>
+
+				<ExperienceCard category="Internship" periodStart="2022" periodEnd="2023">
+					<div slot="title">Internship at PT. Pineleng Indah Cemerlang</div>
+
+					<div slot="description">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi explicabo nostrum
+						quibusdam, esse quo debitis, distinctio molestiae laborum excepturi pariatur est impedit
+						quas quos eos ipsa, voluptate quisquam iste quasi expedita. Hic incidunt totam
+						recusandae molestiae earum fugiat, necessitatibus saepe error sed dolorum numquam eum
+						ratione repellat dolor maxime est!
+					</div>
+				</ExperienceCard>
+			</div>
+		</section>
+
+		<section id="experiences-pagination">
+			<Pagination currentPage={1} totalPage={3} withOutline={true} />
+		</section>
+	</div>
+</main>
+
+<style lang="postcss">
+	@reference "tailwindcss";
+
+	#search-input {
+		@apply flex items-center gap-[0.5rem];
+	}
+
+	#experiences .experiences-grid {
+		@apply grid md:grid-cols-2 gap-[1rem];
+	}
+
+	#experiences-pagination {
+		@apply flex justify-center;
+	}
+</style>
