94e30dbb7700773ca755901609803e840cb3d24c
Nacxht <zombiecrafter069@gmail.com>
2025-07-07 16:36:10 +0700

    feat: added techstack page

---
+++ src/lib/components/reusable/techstack-card/techstack-card.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	export let iconSVGs: string[][];
+</script>
+
+<div class="container">
+	<div class="title-desc">
+		<h3 class="title">
+			<slot name="title" />
+		</h3>
+
+		<p>
+			<slot name="description" />
+		</p>
+	</div>
+
+	<hr class="divider" />
+
+	<div class="techstack-grid">
+		{#each iconSVGs as svg}
+			<div class="tech">
+				{@html svg[1]}
+			</div>
+		{/each}
+	</div>
+</div>
+
+<style lang="postcss">
+	@reference "tailwindcss";
+
+	.container {
+		@apply border border-gray-800 rounded-md p-[1.5rem] grid
+        gap-[1rem];
+	}
+
+	:global(.light) .container {
+		@apply border-gray-300;
+	}
+
+	.container .title-desc {
+		@apply grid gap-[0.5rem];
+	}
+
+	.container .title-desc h3,
+	.container .title-desc p {
+		@apply text-center;
+	}
+
+	.container .techstack-grid {
+		@apply flex flex-wrap gap-[1rem];
+		/* @apply grid grid-cols-8 gap-[1rem] justify-items-center; */
+	}
+
+	.container .techstack-grid .tech {
+		@apply aspect-square bg-gray-800 text-white w-[3.5rem] p-[0.5rem]
+        rounded-md flex justify-center items-center mx-auto;
+	}
+
+	:global(.light) .container .techstack-grid .tech {
+		@apply bg-gray-300 text-black;
+	}
+</style>
---
+++ src/routes/techstack/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import TechstackCard from "$lib/components/reusable/techstack-card/techstack-card.svelte";
+
+	export let data;
+	const { svgs } = data;
+</script>
+
+<main>
+	<div class="container">
+		<section id="title">
+			<h1>Tech Stack</h1>
+		</section>
+
+		<hr class="divider" />
+
+		<section id="currently-used">
+			<TechstackCard iconSVGs={svgs}>
+				<div slot="title">Currently Use</div>
+
+				<div slot="description">The tech stack i currently use right now</div>
+			</TechstackCard>
+		</section>
+
+		<section id="want-learn">
+			<TechstackCard iconSVGs={svgs}>
+				<div slot="title">I want to learn</div>
+
+				<div slot="description">The things i really want to try or learn</div>
+			</TechstackCard>
+		</section>
+	</div>
+</main>
+
+<style lang="postcss">
+	@reference "tailwindcss";
+</style>
