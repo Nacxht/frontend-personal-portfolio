45c468070bda64965df8f88b869c6ddf77acc3e0
Nacxht <zombiecrafter069@gmail.com>
2025-07-07 16:40:36 +0700

    refactor: make the techstack section as a component

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -1,151 +1,62 @@
-<script>
-	// your script goes here
+<script lang="ts">
+	import TechstackCard from "$lib/components/reusable/techstack-card/techstack-card.svelte";
+
+	export let data;
+	const { svgs } = data;
 </script>
 
 <main>
-	<section id="thumbnail-title">
-		<img class="thumbnail" src="https://picsum.photos/1280/720" alt="lorem-ipsum" />
-		<h2>Lorem ipsum dolor sit amet.</h2>
-	</section>
+	<div class="container">
+		<section id="thumbnail-title">
+			<img class="thumbnail" src="https://picsum.photos/1280/720" alt="lorem-ipsum" />
+			<h2>Lorem ipsum dolor sit amet.</h2>
+		</section>
 
-	<hr class="divider" />
+		<hr class="divider" />
 
-	<section id="project-description">
-		<p class="description">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. In, perspiciatis. Neque, blanditiis!
-			Vero sunt totam iusto maxime possimus cum non, iste voluptates assumenda soluta dolorem
-			eligendi labore doloribus distinctio culpa autem quis ea, quo temporibus exercitationem
-			sapiente corrupti reiciendis. Corrupti consequatur voluptates suscipit ad officiis asperiores
-			maxime similique itaque placeat blanditiis officia enim, mollitia ipsum cum dicta molestias
-			perspiciatis obcaecati esse commodi odit? Sed magni maiores quo porro nemo dolore culpa hic,
-			rem doloremque soluta, molestias recusandae similique quam aut deserunt ipsa repellendus
-			laboriosam eos. Quos dolor officia beatae, libero voluptas pariatur obcaecati quo ipsum nemo
-			accusamus voluptatem natus fugiat dicta ex, eaque ad quam aperiam error repellendus inventore
-			impedit! Eaque at excepturi earum deserunt perspiciatis, nobis quisquam, iusto ipsam, aliquid
-			quaerat aperiam dolores porro quia quidem vero soluta rerum velit? Praesentium rerum minus
-			inventore nihil reiciendis culpa similique nesciunt. Iste rerum totam a ullam! Blanditiis
-			libero officia in repellat, maiores, perferendis enim saepe harum consectetur veritatis quia
-			molestiae, numquam ullam earum excepturi modi fugit. A iste dignissimos rem ullam minus
-			explicabo, soluta pariatur magnam temporibus aspernatur doloremque veniam accusantium sequi
-			tempora nostrum officia voluptates similique illum hic esse suscipit. Distinctio laboriosam
-			obcaecati temporibus officiis dolorum id neque eveniet ex!
+		<section id="project-description">
+			<p class="description">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. In, perspiciatis. Neque,
+				blanditiis! Vero sunt totam iusto maxime possimus cum non, iste voluptates assumenda soluta
+				dolorem eligendi labore doloribus distinctio culpa autem quis ea, quo temporibus
+				exercitationem sapiente corrupti reiciendis. Corrupti consequatur voluptates suscipit ad
+				officiis asperiores maxime similique itaque placeat blanditiis officia enim, mollitia ipsum
+				cum dicta molestias perspiciatis obcaecati esse commodi odit? Sed magni maiores quo porro
+				nemo dolore culpa hic, rem doloremque soluta, molestias recusandae similique quam aut
+				deserunt ipsa repellendus laboriosam eos. Quos dolor officia beatae, libero voluptas
+				pariatur obcaecati quo ipsum nemo accusamus voluptatem natus fugiat dicta ex, eaque ad quam
+				aperiam error repellendus inventore impedit! Eaque at excepturi earum deserunt perspiciatis,
+				nobis quisquam, iusto ipsam, aliquid quaerat aperiam dolores porro quia quidem vero soluta
+				rerum velit? Praesentium rerum minus inventore nihil reiciendis culpa similique nesciunt.
+				Iste rerum totam a ullam! Blanditiis libero officia in repellat, maiores, perferendis enim
+				saepe harum consectetur veritatis quia molestiae, numquam ullam earum excepturi modi fugit.
+				A iste dignissimos rem ullam minus explicabo, soluta pariatur magnam temporibus aspernatur
+				doloremque veniam accusantium sequi tempora nostrum officia voluptates similique illum hic
+				esse suscipit. Distinctio laboriosam obcaecati temporibus officiis dolorum id neque eveniet
+				ex!
 
-			<br /><br />
+				<br /><br />
 
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, fugit eum repellendus ipsum
-			consequatur omnis nesciunt asperiores recusandae consequuntur, tempora quos necessitatibus
-			maxime doloribus. Amet recusandae nobis rem enim nostrum esse eligendi expedita, nihil cum
-			nam? Cumque, nostrum? Fugiat deleniti voluptatum perspiciatis quis iusto! Quaerat pariatur
-			assumenda unde ipsa cupiditate enim, neque perferendis nam, exercitationem accusantium nemo
-			laboriosam omnis ex, natus adipisci nostrum facilis porro ad officia quam fuga in possimus
-			fugiat. Officiis modi dolore, exercitationem omnis laudantium nam ullam dicta deserunt. Nulla
-			debitis rem aliquid inventore ducimus cupiditate, reiciendis perferendis quia beatae.
-			Exercitationem, tempore. In nulla quibusdam magnam incidunt!
-		</p>
-	</section>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, fugit eum repellendus ipsum
+				consequatur omnis nesciunt asperiores recusandae consequuntur, tempora quos necessitatibus
+				maxime doloribus. Amet recusandae nobis rem enim nostrum esse eligendi expedita, nihil cum
+				nam? Cumque, nostrum? Fugiat deleniti voluptatum perspiciatis quis iusto! Quaerat pariatur
+				assumenda unde ipsa cupiditate enim, neque perferendis nam, exercitationem accusantium nemo
+				laboriosam omnis ex, natus adipisci nostrum facilis porro ad officia quam fuga in possimus
+				fugiat. Officiis modi dolore, exercitationem omnis laudantium nam ullam dicta deserunt.
+				Nulla debitis rem aliquid inventore ducimus cupiditate, reiciendis perferendis quia beatae.
+				Exercitationem, tempore. In nulla quibusdam magnam incidunt!
+			</p>
+		</section>
 
-	<section id="techstack">
-		<div class="techstack-container">
-			<div class="title-desc">
-				<h3 class="techstack-title">Tech Stack</h3>
-				<p>Tools & Frameworks I used to build this project</p>
-			</div>
+		<section id="techstack">
+			<TechstackCard iconSVGs={svgs}>
+				<div slot="title">Tech Stack</div>
 
-			<hr class="divider" />
-
-			<div class="techstack-grid">
-				<div class="tech">
-					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-						<path
-							fill="currentColor"
-							d="M16.5 6.08s-6.84 1.71-3.56 5.48c.97 1.11-.25 2.11-.25 2.11s2.45-1.25 1.31-2.85c-1.06-1.47-1.86-2.2 2.5-4.74m-4.47 1.2C16.08 4.08 14 2 14 2c.84 3.3-2.96 4.3-4.33 6.36c-.94 1.4.46 2.91 2.33 4.64c-.71-1.7-3.22-3.16.03-5.72M9.37 17.47c-3.08.86 1.88 2.63 5.79.96c-.38-.15-.75-.33-1.1-.54c-1.36.31-2.76.37-4.14.18c-1.31-.16-.55-.6-.55-.6m5.32-1.68c-1.75.38-3.56.47-5.34.26c-1.31-.13-.45-.77-.45-.77c-3.4 1.13 1.88 2.4 6.6 1.02c-.29-.11-.57-.3-.81-.51m3.42 3.3s.57.47-.61.83c-2.28.68-9.43.89-11.41.03c-.71-.31.63-.74 1.05-.83c.23-.06.46-.08.69-.08c-.79-.54-5.13 1.1-2.19 1.56c7.97 1.3 14.54-.6 12.47-1.51m-2.74-4.86c.29-.19.6-.35.92-.49c0 0-1.51.26-3.02.4c-1.6.16-3.21.18-4.81.06c-2.35-.31 1.29-1.2 1.29-1.2c-1.1 0-2.18.26-3.16.75c-2.05 1 5.1 1.45 8.78.48m.9 2.42c-.02.04-.04.07-.08.1c5.01-1.31 3.17-4.64.77-3.81c-.13.06-.24.14-.31.25c.14-.05.28-.09.43-.12c1.2-.24 2.92 1.63-.81 3.58m.13 4.61c-3.01.52-6.09.56-9.12.14c0 0 .46.38 2.81.53c3.6.23 9.13-.13 9.26-1.83c.03.01-.23.65-2.95 1.16"
-						/>
-					</svg>
-				</div>
-
-				<div class="tech">
-					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-						<path
-							fill="currentColor"
-							d="M16.5 6.08s-6.84 1.71-3.56 5.48c.97 1.11-.25 2.11-.25 2.11s2.45-1.25 1.31-2.85c-1.06-1.47-1.86-2.2 2.5-4.74m-4.47 1.2C16.08 4.08 14 2 14 2c.84 3.3-2.96 4.3-4.33 6.36c-.94 1.4.46 2.91 2.33 4.64c-.71-1.7-3.22-3.16.03-5.72M9.37 17.47c-3.08.86 1.88 2.63 5.79.96c-.38-.15-.75-.33-1.1-.54c-1.36.31-2.76.37-4.14.18c-1.31-.16-.55-.6-.55-.6m5.32-1.68c-1.75.38-3.56.47-5.34.26c-1.31-.13-.45-.77-.45-.77c-3.4 1.13 1.88 2.4 6.6 1.02c-.29-.11-.57-.3-.81-.51m3.42 3.3s.57.47-.61.83c-2.28.68-9.43.89-11.41.03c-.71-.31.63-.74 1.05-.83c.23-.06.46-.08.69-.08c-.79-.54-5.13 1.1-2.19 1.56c7.97 1.3 14.54-.6 12.47-1.51m-2.74-4.86c.29-.19.6-.35.92-.49c0 0-1.51.26-3.02.4c-1.6.16-3.21.18-4.81.06c-2.35-.31 1.29-1.2 1.29-1.2c-1.1 0-2.18.26-3.16.75c-2.05 1 5.1 1.45 8.78.48m.9 2.42c-.02.04-.04.07-.08.1c5.01-1.31 3.17-4.64.77-3.81c-.13.06-.24.14-.31.25c.14-.05.28-.09.43-.12c1.2-.24 2.92 1.63-.81 3.58m.13 4.61c-3.01.52-6.09.56-9.12.14c0 0 .46.38 2.81.53c3.6.23 9.13-.13 9.26-1.83c.03.01-.23.65-2.95 1.16"
-						/>
-					</svg>
-				</div>
-
-				<div class="tech">
-					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-						<path
-							fill="currentColor"
-							d="M16.5 6.08s-6.84 1.71-3.56 5.48c.97 1.11-.25 2.11-.25 2.11s2.45-1.25 1.31-2.85c-1.06-1.47-1.86-2.2 2.5-4.74m-4.47 1.2C16.08 4.08 14 2 14 2c.84 3.3-2.96 4.3-4.33 6.36c-.94 1.4.46 2.91 2.33 4.64c-.71-1.7-3.22-3.16.03-5.72M9.37 17.47c-3.08.86 1.88 2.63 5.79.96c-.38-.15-.75-.33-1.1-.54c-1.36.31-2.76.37-4.14.18c-1.31-.16-.55-.6-.55-.6m5.32-1.68c-1.75.38-3.56.47-5.34.26c-1.31-.13-.45-.77-.45-.77c-3.4 1.13 1.88 2.4 6.6 1.02c-.29-.11-.57-.3-.81-.51m3.42 3.3s.57.47-.61.83c-2.28.68-9.43.89-11.41.03c-.71-.31.63-.74 1.05-.83c.23-.06.46-.08.69-.08c-.79-.54-5.13 1.1-2.19 1.56c7.97 1.3 14.54-.6 12.47-1.51m-2.74-4.86c.29-.19.6-.35.92-.49c0 0-1.51.26-3.02.4c-1.6.16-3.21.18-4.81.06c-2.35-.31 1.29-1.2 1.29-1.2c-1.1 0-2.18.26-3.16.75c-2.05 1 5.1 1.45 8.78.48m.9 2.42c-.02.04-.04.07-.08.1c5.01-1.31 3.17-4.64.77-3.81c-.13.06-.24.14-.31.25c.14-.05.28-.09.43-.12c1.2-.24 2.92 1.63-.81 3.58m.13 4.61c-3.01.52-6.09.56-9.12.14c0 0 .46.38 2.81.53c3.6.23 9.13-.13 9.26-1.83c.03.01-.23.65-2.95 1.16"
-						/>
-					</svg>
-				</div>
-
-				<div class="tech">
-					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-						<path
-							fill="currentColor"
-							d="M16.5 6.08s-6.84 1.71-3.56 5.48c.97 1.11-.25 2.11-.25 2.11s2.45-1.25 1.31-2.85c-1.06-1.47-1.86-2.2 2.5-4.74m-4.47 1.2C16.08 4.08 14 2 14 2c.84 3.3-2.96 4.3-4.33 6.36c-.94 1.4.46 2.91 2.33 4.64c-.71-1.7-3.22-3.16.03-5.72M9.37 17.47c-3.08.86 1.88 2.63 5.79.96c-.38-.15-.75-.33-1.1-.54c-1.36.31-2.76.37-4.14.18c-1.31-.16-.55-.6-.55-.6m5.32-1.68c-1.75.38-3.56.47-5.34.26c-1.31-.13-.45-.77-.45-.77c-3.4 1.13 1.88 2.4 6.6 1.02c-.29-.11-.57-.3-.81-.51m3.42 3.3s.57.47-.61.83c-2.28.68-9.43.89-11.41.03c-.71-.31.63-.74 1.05-.83c.23-.06.46-.08.69-.08c-.79-.54-5.13 1.1-2.19 1.56c7.97 1.3 14.54-.6 12.47-1.51m-2.74-4.86c.29-.19.6-.35.92-.49c0 0-1.51.26-3.02.4c-1.6.16-3.21.18-4.81.06c-2.35-.31 1.29-1.2 1.29-1.2c-1.1 0-2.18.26-3.16.75c-2.05 1 5.1 1.45 8.78.48m.9 2.42c-.02.04-.04.07-.08.1c5.01-1.31 3.17-4.64.77-3.81c-.13.06-.24.14-.31.25c.14-.05.28-.09.43-.12c1.2-.24 2.92 1.63-.81 3.58m.13 4.61c-3.01.52-6.09.56-9.12.14c0 0 .46.38 2.81.53c3.6.23 9.13-.13 9.26-1.83c.03.01-.23.65-2.95 1.16"
-						/>
-					</svg>
-				</div>
-
-				<div class="tech">
-					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-						<path
-							fill="currentColor"
-							d="M16.5 6.08s-6.84 1.71-3.56 5.48c.97 1.11-.25 2.11-.25 2.11s2.45-1.25 1.31-2.85c-1.06-1.47-1.86-2.2 2.5-4.74m-4.47 1.2C16.08 4.08 14 2 14 2c.84 3.3-2.96 4.3-4.33 6.36c-.94 1.4.46 2.91 2.33 4.64c-.71-1.7-3.22-3.16.03-5.72M9.37 17.47c-3.08.86 1.88 2.63 5.79.96c-.38-.15-.75-.33-1.1-.54c-1.36.31-2.76.37-4.14.18c-1.31-.16-.55-.6-.55-.6m5.32-1.68c-1.75.38-3.56.47-5.34.26c-1.31-.13-.45-.77-.45-.77c-3.4 1.13 1.88 2.4 6.6 1.02c-.29-.11-.57-.3-.81-.51m3.42 3.3s.57.47-.61.83c-2.28.68-9.43.89-11.41.03c-.71-.31.63-.74 1.05-.83c.23-.06.46-.08.69-.08c-.79-.54-5.13 1.1-2.19 1.56c7.97 1.3 14.54-.6 12.47-1.51m-2.74-4.86c.29-.19.6-.35.92-.49c0 0-1.51.26-3.02.4c-1.6.16-3.21.18-4.81.06c-2.35-.31 1.29-1.2 1.29-1.2c-1.1 0-2.18.26-3.16.75c-2.05 1 5.1 1.45 8.78.48m.9 2.42c-.02.04-.04.07-.08.1c5.01-1.31 3.17-4.64.77-3.81c-.13.06-.24.14-.31.25c.14-.05.28-.09.43-.12c1.2-.24 2.92 1.63-.81 3.58m.13 4.61c-3.01.52-6.09.56-9.12.14c0 0 .46.38 2.81.53c3.6.23 9.13-.13 9.26-1.83c.03.01-.23.65-2.95 1.16"
-						/>
-					</svg>
-				</div>
-
-				<div class="tech">
-					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-						<path
-							fill="currentColor"
-							d="M16.5 6.08s-6.84 1.71-3.56 5.48c.97 1.11-.25 2.11-.25 2.11s2.45-1.25 1.31-2.85c-1.06-1.47-1.86-2.2 2.5-4.74m-4.47 1.2C16.08 4.08 14 2 14 2c.84 3.3-2.96 4.3-4.33 6.36c-.94 1.4.46 2.91 2.33 4.64c-.71-1.7-3.22-3.16.03-5.72M9.37 17.47c-3.08.86 1.88 2.63 5.79.96c-.38-.15-.75-.33-1.1-.54c-1.36.31-2.76.37-4.14.18c-1.31-.16-.55-.6-.55-.6m5.32-1.68c-1.75.38-3.56.47-5.34.26c-1.31-.13-.45-.77-.45-.77c-3.4 1.13 1.88 2.4 6.6 1.02c-.29-.11-.57-.3-.81-.51m3.42 3.3s.57.47-.61.83c-2.28.68-9.43.89-11.41.03c-.71-.31.63-.74 1.05-.83c.23-.06.46-.08.69-.08c-.79-.54-5.13 1.1-2.19 1.56c7.97 1.3 14.54-.6 12.47-1.51m-2.74-4.86c.29-.19.6-.35.92-.49c0 0-1.51.26-3.02.4c-1.6.16-3.21.18-4.81.06c-2.35-.31 1.29-1.2 1.29-1.2c-1.1 0-2.18.26-3.16.75c-2.05 1 5.1 1.45 8.78.48m.9 2.42c-.02.04-.04.07-.08.1c5.01-1.31 3.17-4.64.77-3.81c-.13.06-.24.14-.31.25c.14-.05.28-.09.43-.12c1.2-.24 2.92 1.63-.81 3.58m.13 4.61c-3.01.52-6.09.56-9.12.14c0 0 .46.38 2.81.53c3.6.23 9.13-.13 9.26-1.83c.03.01-.23.65-2.95 1.16"
-						/>
-					</svg>
-				</div>
-
-				<div class="tech">
-					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-						<path
-							fill="currentColor"
-							d="M16.5 6.08s-6.84 1.71-3.56 5.48c.97 1.11-.25 2.11-.25 2.11s2.45-1.25 1.31-2.85c-1.06-1.47-1.86-2.2 2.5-4.74m-4.47 1.2C16.08 4.08 14 2 14 2c.84 3.3-2.96 4.3-4.33 6.36c-.94 1.4.46 2.91 2.33 4.64c-.71-1.7-3.22-3.16.03-5.72M9.37 17.47c-3.08.86 1.88 2.63 5.79.96c-.38-.15-.75-.33-1.1-.54c-1.36.31-2.76.37-4.14.18c-1.31-.16-.55-.6-.55-.6m5.32-1.68c-1.75.38-3.56.47-5.34.26c-1.31-.13-.45-.77-.45-.77c-3.4 1.13 1.88 2.4 6.6 1.02c-.29-.11-.57-.3-.81-.51m3.42 3.3s.57.47-.61.83c-2.28.68-9.43.89-11.41.03c-.71-.31.63-.74 1.05-.83c.23-.06.46-.08.69-.08c-.79-.54-5.13 1.1-2.19 1.56c7.97 1.3 14.54-.6 12.47-1.51m-2.74-4.86c.29-.19.6-.35.92-.49c0 0-1.51.26-3.02.4c-1.6.16-3.21.18-4.81.06c-2.35-.31 1.29-1.2 1.29-1.2c-1.1 0-2.18.26-3.16.75c-2.05 1 5.1 1.45 8.78.48m.9 2.42c-.02.04-.04.07-.08.1c5.01-1.31 3.17-4.64.77-3.81c-.13.06-.24.14-.31.25c.14-.05.28-.09.43-.12c1.2-.24 2.92 1.63-.81 3.58m.13 4.61c-3.01.52-6.09.56-9.12.14c0 0 .46.38 2.81.53c3.6.23 9.13-.13 9.26-1.83c.03.01-.23.65-2.95 1.16"
-						/>
-					</svg>
-				</div>
-
-				<div class="tech">
-					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-						<path
-							fill="currentColor"
-							d="M16.5 6.08s-6.84 1.71-3.56 5.48c.97 1.11-.25 2.11-.25 2.11s2.45-1.25 1.31-2.85c-1.06-1.47-1.86-2.2 2.5-4.74m-4.47 1.2C16.08 4.08 14 2 14 2c.84 3.3-2.96 4.3-4.33 6.36c-.94 1.4.46 2.91 2.33 4.64c-.71-1.7-3.22-3.16.03-5.72M9.37 17.47c-3.08.86 1.88 2.63 5.79.96c-.38-.15-.75-.33-1.1-.54c-1.36.31-2.76.37-4.14.18c-1.31-.16-.55-.6-.55-.6m5.32-1.68c-1.75.38-3.56.47-5.34.26c-1.31-.13-.45-.77-.45-.77c-3.4 1.13 1.88 2.4 6.6 1.02c-.29-.11-.57-.3-.81-.51m3.42 3.3s.57.47-.61.83c-2.28.68-9.43.89-11.41.03c-.71-.31.63-.74 1.05-.83c.23-.06.46-.08.69-.08c-.79-.54-5.13 1.1-2.19 1.56c7.97 1.3 14.54-.6 12.47-1.51m-2.74-4.86c.29-.19.6-.35.92-.49c0 0-1.51.26-3.02.4c-1.6.16-3.21.18-4.81.06c-2.35-.31 1.29-1.2 1.29-1.2c-1.1 0-2.18.26-3.16.75c-2.05 1 5.1 1.45 8.78.48m.9 2.42c-.02.04-.04.07-.08.1c5.01-1.31 3.17-4.64.77-3.81c-.13.06-.24.14-.31.25c.14-.05.28-.09.43-.12c1.2-.24 2.92 1.63-.81 3.58m.13 4.61c-3.01.52-6.09.56-9.12.14c0 0 .46.38 2.81.53c3.6.23 9.13-.13 9.26-1.83c.03.01-.23.65-2.95 1.16"
-						/>
-					</svg>
-				</div>
-
-				<div class="tech">
-					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-						<path
-							fill="currentColor"
-							d="M16.5 6.08s-6.84 1.71-3.56 5.48c.97 1.11-.25 2.11-.25 2.11s2.45-1.25 1.31-2.85c-1.06-1.47-1.86-2.2 2.5-4.74m-4.47 1.2C16.08 4.08 14 2 14 2c.84 3.3-2.96 4.3-4.33 6.36c-.94 1.4.46 2.91 2.33 4.64c-.71-1.7-3.22-3.16.03-5.72M9.37 17.47c-3.08.86 1.88 2.63 5.79.96c-.38-.15-.75-.33-1.1-.54c-1.36.31-2.76.37-4.14.18c-1.31-.16-.55-.6-.55-.6m5.32-1.68c-1.75.38-3.56.47-5.34.26c-1.31-.13-.45-.77-.45-.77c-3.4 1.13 1.88 2.4 6.6 1.02c-.29-.11-.57-.3-.81-.51m3.42 3.3s.57.47-.61.83c-2.28.68-9.43.89-11.41.03c-.71-.31.63-.74 1.05-.83c.23-.06.46-.08.69-.08c-.79-.54-5.13 1.1-2.19 1.56c7.97 1.3 14.54-.6 12.47-1.51m-2.74-4.86c.29-.19.6-.35.92-.49c0 0-1.51.26-3.02.4c-1.6.16-3.21.18-4.81.06c-2.35-.31 1.29-1.2 1.29-1.2c-1.1 0-2.18.26-3.16.75c-2.05 1 5.1 1.45 8.78.48m.9 2.42c-.02.04-.04.07-.08.1c5.01-1.31 3.17-4.64.77-3.81c-.13.06-.24.14-.31.25c.14-.05.28-.09.43-.12c1.2-.24 2.92 1.63-.81 3.58m.13 4.61c-3.01.52-6.09.56-9.12.14c0 0 .46.38 2.81.53c3.6.23 9.13-.13 9.26-1.83c.03.01-.23.65-2.95 1.16"
-						/>
-					</svg>
-				</div>
-
-				<div class="tech">
-					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-						<path
-							fill="currentColor"
-							d="M16.5 6.08s-6.84 1.71-3.56 5.48c.97 1.11-.25 2.11-.25 2.11s2.45-1.25 1.31-2.85c-1.06-1.47-1.86-2.2 2.5-4.74m-4.47 1.2C16.08 4.08 14 2 14 2c.84 3.3-2.96 4.3-4.33 6.36c-.94 1.4.46 2.91 2.33 4.64c-.71-1.7-3.22-3.16.03-5.72M9.37 17.47c-3.08.86 1.88 2.63 5.79.96c-.38-.15-.75-.33-1.1-.54c-1.36.31-2.76.37-4.14.18c-1.31-.16-.55-.6-.55-.6m5.32-1.68c-1.75.38-3.56.47-5.34.26c-1.31-.13-.45-.77-.45-.77c-3.4 1.13 1.88 2.4 6.6 1.02c-.29-.11-.57-.3-.81-.51m3.42 3.3s.57.47-.61.83c-2.28.68-9.43.89-11.41.03c-.71-.31.63-.74 1.05-.83c.23-.06.46-.08.69-.08c-.79-.54-5.13 1.1-2.19 1.56c7.97 1.3 14.54-.6 12.47-1.51m-2.74-4.86c.29-.19.6-.35.92-.49c0 0-1.51.26-3.02.4c-1.6.16-3.21.18-4.81.06c-2.35-.31 1.29-1.2 1.29-1.2c-1.1 0-2.18.26-3.16.75c-2.05 1 5.1 1.45 8.78.48m.9 2.42c-.02.04-.04.07-.08.1c5.01-1.31 3.17-4.64.77-3.81c-.13.06-.24.14-.31.25c.14-.05.28-.09.43-.12c1.2-.24 2.92 1.63-.81 3.58m.13 4.61c-3.01.52-6.09.56-9.12.14c0 0 .46.38 2.81.53c3.6.23 9.13-.13 9.26-1.83c.03.01-.23.65-2.95 1.16"
-						/>
-					</svg>
-				</div>
-			</div>
-		</div>
-	</section>
+				<div slot="description">Tools & Frameworks I used to build this project</div>
+			</TechstackCard>
+		</section>
+	</div>
 </main>
 
 <style lang="postcss">
@@ -161,36 +72,5 @@
 
 	#thumbnail-title h2 {
 		@apply text-center;
-	}
-
-	#techstack .techstack-container {
-		@apply border border-gray-800 rounded-md p-[1.5rem] grid
-		gap-[1rem];
-	}
-
-	:global(.light) #techstack .techstack-container {
-		@apply border-gray-300;
-	}
-
-	#techstack .techstack-container .title-desc {
-		@apply grid gap-[0.5rem];
-	}
-
-	#techstack .techstack-container .title-desc h3,
-	#techstack .techstack-container .title-desc p {
-		@apply text-center;
-	}
-
-	#techstack .techstack-container .techstack-grid {
-		@apply flex flex-wrap gap-[1rem];
-	}
-
-	#techstack .techstack-container .techstack-grid .tech {
-		@apply aspect-square bg-gray-800 text-white w-[3.5rem] p-[0.5rem]
-		rounded-md flex justify-center items-center mx-auto;
-	}
-
-	:global(.light) #techstack .techstack-container .techstack-grid .tech {
-		@apply bg-gray-300 text-black;
 	}
 </style>
